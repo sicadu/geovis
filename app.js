5e32a07faf6455aca0733ba5e953d68c7fb48d5a
anninaardueser <annina.ardueser@uzh.ch>
2022-05-04 11:07:00 +0200

    Scatterplot + neue Daten

--- app.js
+++ app.js
@@ -11,26 +11,19 @@ import {StaticMap} from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
 import {scaleQuantile} from 'd3-scale';
-// for Icon Layer: import {IconLayer} from '@deck.gl/layers';
-// used in Scatterplot Script: import {DataFilterExtension} from '@deck.gl/extensions';
+//import {IconLayer} from '@deck.gl/layers';
 import {ScatterplotLayer} from '@deck.gl/layers';
 
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { PowerTwoTone } from '@material-ui/icons';
 
+// Remittance datasets: Declare json to variables
+const remi_in = './data/remittances_IN_PROP_WGS84.geojson'
 
 // Source data GeoJSON
-//const DATA_URL =
-  //'./data/e15.json'; // eslint-disable-line
+const DATA_URL =
+  './data/e15.json'; // eslint-disable-line
 
-// Source data PROP_IN
-const DATA_URL_IN = 
-'./data/remittances_IN_PROP_centroid_millions.json';  // eslint-disable-line
-// Source data PROP_OUT
-//const DATA_URL_OUT = 
-//'./data/remittances_OUT_PROP_centroid_millions.json';  // eslint-disable-line
-
+//comment to delete
 
   export const inFlowColors = [
     [255, 255, 204],
@@ -57,14 +50,12 @@ const DATA_URL_IN =
     latitude: 50,
     zoom: 3,
     maxZoom: 15,
-    pitch: 0,
+    pitch: 30, // muss 30 sein, damit arcs schön dargestellt werden können
     bearing: 0
   };
   
   const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
-/* 
-////////////////////////////////////////////////////////////////
-  // functions Flows
+  
   function calculateArcs(data, selectedCounty) {
     console.log(data);
     if (!data || !data.length) {
@@ -100,27 +91,9 @@ const DATA_URL_IN =
   function getTooltip({object}) {
     return object && object.properties.Country;
   }
-*/
-////////////////////////////////////////////////////////////////
-  // functions Proportional Symbols
-
-  // Pop-up
-function getTooltip({object}) {
-  return (
-    object &&
-    `\
-    Country: ${object.COUNTRY}
-    GDP: ${object.GDP_2017}
-    `
-  );
-}
   
-
-
-  ////////////////////////////////////////////////////////////////////////
-  // Add all Layers
   /* eslint-disable react/no-deprecated */
-  /*export default function App({data, strokeWidth = 1, mapStyle = MAP_STYLE}) {
+  export default function App({data, strokeWidth = 1, mapStyle = MAP_STYLE}) {
     const [selectedCounty, selectCounty] = useState(null);
   
     const arcs = useMemo(() => calculateArcs(data, selectedCounty), [data, selectedCounty]);
@@ -143,42 +116,16 @@ function getTooltip({object}) {
         getSourceColor: d => (d.gain > 0 ? inFlowColors : outFlowColors)[d.quantile],
         getTargetColor: d => (d.gain > 0 ? outFlowColors : inFlowColors)[d.quantile],
         getWidth: strokeWidth
-      })
-      
-    ];
-*/
-export default function App({data, mapStyle = MAP_STYLE}) {
-  const [filter, setFilter] = useState(null);
-
-  const layers = [
-    data &&
+      }),
+      // create proportional symbols layers
       new ScatterplotLayer({
-        id: 'proportional',
-        data,
-        opacity: 0.8,
-        radiusScale: 100,
-        radiusMinPixels: 1,
-        wrapLongitude: true,
-
-        //Formel für Farbe!
-        getPosition: d => [d.X, d.Y],
-        getRadius: d =>  Math.sqrt(d.GDP_2017)/Math.Pi,
-        getFillColor: d => {
-          const r = ((d.R_IN_2017) * Math.pow(10,6))/d.GDP_2017; //als variabel
-          return [255 - r * 15, r * 5, r * 10]; //werte für Farbschema ändern
-        }
-
-        /*getFilterValue: d => d.timestamp,
-        filterRange: [filterValue[0], filterValue[1]],
-        filterSoftRange: [
-          filterValue[0] * 0.9 + filterValue[1] * 0.1,
-          filterValue[0] * 0.1 + filterValue[1] * 0.9*/
-        //],
-        //extensions: [dataFilter], //evtl weg
-
-        //pickable: true
+        data: remi_in,
+        getPosition: d => [d.properties.X, d.properties.Y], // Koordinaten werden noch nicht richtig ausgewählt
+        getRadius: 10,
+        getColor: [155, 40, 0],
+        radiusMinPixels: 2
       })
-  ];
+    ];
 
   return (
     <>
@@ -203,7 +150,7 @@ export default function App({data, mapStyle = MAP_STYLE}) {
       <div className="intro page" id="introNav">
         <div className="e">Remittance and Migration Flows: </div> 
         <div className="e"> 
-            <span className="s"> Development of Global Pattern</span>
+            <span className="s">Global Patterns</span>
         </div>
       </div>
       <div className="background page" id="backgroundNav">Background</div>
@@ -261,15 +208,12 @@ export default function App({data, mapStyle = MAP_STYLE}) {
   );
 }
 
-  export function renderToDOM(container) {
+export function renderToDOM(container) {
   render(<App />, container);
 
-  fetch(DATA_URL_IN)
+  fetch(DATA_URL)
     .then(response => response.json())
     .then(({features}) => {
       render(<App data={features} />, container);
     });
 }
-
-
-
